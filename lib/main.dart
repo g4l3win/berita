a215a2dd2e3bfbcd1cab39cdb8caac6b51cdd3c6
Acer <Acer@LAPTOP-FTNPN94M>
2024-09-28 15:53:45 +0700

    cek

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const CarouselPage(), //HomeScreen(),
+      home: const CarouselPage(), //HomeScreen(),lkkl
     );
   }
 }
